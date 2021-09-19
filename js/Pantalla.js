46da2cb6b5fd19bc4f3d14c302627457ae62987e
Jhon Jaime Novoa <jmario160402@gmail.com>
2021-09-19 01:12:49 -0500

    Calculadora Terminada

---
+++ js/Pantalla.js
@@ -0,0 +1,47 @@
+class Pantalla {
+    constructor(pantallaValorPasado, pantallaValorPresente){
+        this.pantallaValorPresente = pantallaValorPresente;
+        this.pantallaValorPasado = pantallaValorPasado;
+        this.calculador = new Calculadora();
+        this.valorPresente = '';
+        this.valorPasado = '';
+        this.tipoOperacion = undefined;
+    }
+
+    agregarNumero(numero){
+        this.valorPresente = this.valorPresente.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores(){
+        this.pantallaValorPresente.textContent = this.valorPresente;
+        this.pantallaValorPasado.textContent = this.valorPasado;
+    }
+
+    borrar(){
+        this.valorPresente = this.valorPresente.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+
+    borrainador(){
+        this.valorPresente = '';
+        this.valorPasado = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+
+    calcular(){
+        const valorPasado = parseFloat(this.valorPasado);
+        const valorPresente = parseFloat(this.valorPresente)
+        if( isNaN(valorPresente) || isNaN(valorPasado) ) return
+        this.valorPresente = this.calculador[this.tipoOperacion](valorPasado, valorPresente);       
+    }
+    computar(tipo){
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorPasado = this.valorPresente || this.valorPasado;
+        this.valorPresente = '';
+        this.imprimirValores();
+
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
-const displayValorPasado = document.getElementById('valor-pasado');
-const displayValorPresente = document.getElementById('valor-presente');
+const pantallaValorPasado = document.getElementById('valor-pasado');
+const pantallaValorPresente = document.getElementById('valor-presente');
 const botonesNumeros = document.querySelectorAll('.numero');
 const botonesOperadores = document.querySelectorAll('.operador');
 
+
+const pantalla = new Pantalla(pantallaValorPasado, pantallaValorPresente);
+
+botonesNumeros.forEach(boton => {
+     boton.addEventListener('click', () => pantalla.agregarNumero(boton.innerHTML));
+}
+);
+
+
+botonesOperadores.forEach(boton =>{
+    boton.addEventListener('click', () => pantalla.computar(boton.value))
+});
 
