4f6527e53c313325855c4e658f96482c3de75592
Jhon Jaime Novoa <jmario160402@gmail.com>
2021-09-18 22:18:12 -0500

    Java Parte Calculadora

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const displayValorPasado = document.getElementById('valor-pasado');
+const displayValorPresente = document.getElementById('valor-presente');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+
